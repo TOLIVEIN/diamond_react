828324f5b88a00f6c81752527e2ffd7c9c4e13e7
TOLIVEIN <TOLIVEIN@outlook.com>
2020-05-22 09:28:51 +0800

    use rxjs throttle

--- src/components/TopJumper/TopJumper.tsx
+++ src/components/TopJumper/TopJumper.tsx
@@ -1,48 +1,79 @@
 import React, { FC } from "react";
 import "./TopJumper.scss";
-import {useState} from 'react';
-import {useEffect} from 'react';
+import { useState } from "react";
+import { useEffect } from "react";
+
+import { throttleTime, filter, map } from "rxjs/operators";
+import { fromEvent } from "rxjs";
 
 const TopJumper: FC<{}> = (props) => {
-
     const [show, setShow] = useState(false);
-
-    const createThrottle = (callback: Function, delay?: number, thisArg?: any): Function => {
-        let lastInvokeTime: number = Date.now();
-        const delay_time = Number(delay) || 200;
-        return (...args: any[]): void => {
-            const now = Date.now();
-            if (now - delay_time <= lastInvokeTime) {
-                return;
-            }
-            lastInvokeTime = now;
-            callback.call(thisArg, ...args);
-        }
-    }
+    // const previousY = 0;
 
     useEffect(() => {
-        const listener = createThrottle(() => {
-            const shouldShow = window.scrollY > 300;
+        const scroll = fromEvent(window, "scroll").pipe(
+            map(() => window.scrollY),
+            filter((_) => window.scrollY> 1000),
+            throttleTime(500)
+        );
+        scroll.subscribe((currentY) => {
+            console.log("Y*********", window.scrollY);
+            const shouldShow = window.scrollY > 1000;
+            console.log(
+                "********************, shouldShow: " +
+                    shouldShow +
+                    ", show: " +
+                    show
+            );
             if (shouldShow !== show) {
+
+
                 setShow(shouldShow);
             }
-        }, 500) as EventListener;
+            // setShow(true);
+        });
+    }, [show]);
 
-        document.addEventListener('scroll', listener);
-        return () => {
-            document.removeEventListener('scroll', listener);
-        }
-    }, [show])
+    // const createThrottle = (callback: Function, delay?: number, thisArg?: any): Function => {
+    //     let lastInvokeTime: number = Date.now();
+    //     const delay_time = Number(delay) || 200;
+    //     return (...args: any[]): void => {
+    //         const now = Date.now();
+    //         if (now - delay_time <= lastInvokeTime) {
+    //             return;
+    //         }
+    //         lastInvokeTime = now;
+    //         callback.call(thisArg, ...args);
+    //     }
+    // }
 
-  return show ? (
-    <>
-        <div className="top-jumper" onClick={() => {
-            window.scrollTo(0, 0)
-        }}>
-            <span className="text"></span>
-        </div>
-    </>
-  ) : null;
+    // useEffect(() => {
+    //     const listener = createThrottle(() => {
+    //         const shouldShow = window.scrollY > 300;
+    //         if (shouldShow !== show) {
+    //             setShow(shouldShow);
+    //         }
+    //     }, 500) as EventListener;
+
+    //     document.addEventListener('scroll', listener);
+    //     return () => {
+    //         document.removeEventListener('scroll', listener);
+    //     }
+    // }, [show])
+
+    return show ? (
+        <>
+            <div
+                className="top-jumper"
+                onClick={() => {
+                    window.scrollTo(0, 0);
+                    // setShow(false);
+                }}
+            >
+                <span className="text"></span>
+            </div>
+        </>
+    ) : null;
 };
 
 export default TopJumper;
