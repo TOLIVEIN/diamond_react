0b0c32aefbee49a8bd917e733324953f6dfcd473
TOLIVEIN <tolivein@outlook.com>
2020-05-27 07:57:11 +0000

    Deploying to gh-pages from  @ cf475b942299494143dab7f44e2bc5c150f3aa70 🚀

---
+++ precache-manifest.8332699ca5c105eac0e37fdd3e436d84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed22e8ed0f6fc85abc4de8813154b62",
+    "url": "/diamond_react/index.html"
+  },
+  {
+    "revision": "e772dcb89c2df25b6296",
+    "url": "/diamond_react/static/css/main.cf51d81f.chunk.css"
+  },
+  {
+    "revision": "09bf191f5b0f38b2e65d",
+    "url": "/diamond_react/static/js/2.59e8510d.chunk.js"
+  },
+  {
+    "revision": "9e290fcffc4b1ab39329767c9b05e1a4",
+    "url": "/diamond_react/static/js/2.59e8510d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e772dcb89c2df25b6296",
+    "url": "/diamond_react/static/js/main.a40006a0.chunk.js"
+  },
+  {
+    "revision": "5faded33f219798b8ab7",
+    "url": "/diamond_react/static/js/runtime-main.4834fd0b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.872ae56278b82f20395c50c161733b39.js"
+  "/diamond_react/precache-manifest.8332699ca5c105eac0e37fdd3e436d84.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/diamond_react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
